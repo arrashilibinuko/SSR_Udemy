8a5b72cd1c498e628243644174db409d9f3d976e
Arrashi <arrashilibinuko.19@ichat.sp.edu.sg>
2020-09-29 19:43:21 +0800

    express setup and boilerplate

---
+++ server/src/client/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <div>Im the test home VERY VERy BEST component</div>
+        </div>
+    );
+};
+
+
+export default Home;
--- server/src/index.js
+++ server/src/index.js
@@ -1,2 +1,22 @@
 const express = require('express');
-tet
+const React = require('react');
+const renderToString = requrie('react-dom/server').renderToString;
+const Home = require('./client/components/Home')
+const app = express();
+
+app.get('*', (req,res) => {
+    // const store = createStore();
+
+    // //some logic to initalize and load data into store  
+    // res.send(renderer(req,store));
+
+    const content = renderToString(<Home></Home>)
+
+
+    res.send(content);
+})
+
+
+app.listen(1000, () => {
+    console.log('Listening on port 3000');
+})
