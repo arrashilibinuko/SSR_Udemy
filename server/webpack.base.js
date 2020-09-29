47b446c87b470eb1e76633d601bf734dbde62464
Arrashi <arrashilibinuko.19@ichat.sp.edu.sg>
2020-09-29 19:50:46 +0800

    jsx wepack server

---
+++ server/webpack.base.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+    //tell webpack to run babel on every file it runs
+    module: {
+    rules: [
+        {
+            test: /\.js?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            options: { 
+                presets: [
+                    'react',
+                    'stage-0',
+                    ['env', {target: {
+                        browsers: [['last 2 versions']]
+                    }}]
+                ]
+            }
+        }
+    ]
+}
+};
