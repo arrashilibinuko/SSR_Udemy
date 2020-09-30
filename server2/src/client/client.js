f6d12597ecf093b0cf6630816c8fc6a641d7c101
Arrashi <arrashilibinuko.19@ichat.sp.edu.sg>
2020-09-30 12:21:36 +0800

    added store in client.js

--- server2/src/client/client.js
+++ server2/src/client/client.js
@@ -3,11 +3,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import Home from './componen;ts/Home';
 import { BrowserRouter } from 'react-router-dom';
-import Routes from './Routes'
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import Routes from './Routes';
+
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 ReactDOM.hydrate(
-    <BrowserRouter>
-        <Routes/>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <Routes/>
+        </BrowserRouter>
+    </Provider>
     , document.querySelector('#root')
 );
