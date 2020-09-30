8114e1057ba9b8485fbb80e8788e65d88abb2be3
Arrashi <arrashilibinuko.19@ichat.sp.edu.sg>
2020-09-30 11:16:35 +0800

    created helper to sperate rendering logic from express logic in index.js

--- server2/build/bundle.js
+++ server2/build/bundle.js
@@ -95,19 +95,13 @@ var _express = __webpack_require__(1);
 
 var _express2 = _interopRequireDefault(_express);
 
-var _react = __webpack_require__(2);
+var _renderer = __webpack_require__(5);
 
-var _react2 = _interopRequireDefault(_react);
-
-var _server = __webpack_require__(3);
-
-var _Home = __webpack_require__(4);
-
-var _Home2 = _interopRequireDefault(_Home);
+var _renderer2 = _interopRequireDefault(_renderer);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-//common JS importation
+// common JS importation
 // const express = require('express');
 // const React = require('react');
 // const renderToString = require('react-dom/server').renderToString;
@@ -118,14 +112,14 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var app = (0, _express2.default)();
 
 //Tells express to treat ./public as static/available to outside
-//is a named exports
+
+// import React from 'react';
+// import { renderToString } from 'react-dom/server'; //is a named exports
+// import Home from './client/components/Home';
 app.use(_express2.default.static('public'));
 
 app.get('*', function (req, res) {
-    var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
-
-    var html = '\n        <html>\n            <head></head>\n            <body>\n                <div id="root">' + content + '</div>\n                <script src="bundle.js"></script>\n            </body>\n        </html>\n    ';
-    res.send(html);
+    res.send((0, _renderer2.default)());
 });
 
 app.listen(3000, function () {
@@ -187,6 +181,35 @@ var Home = function Home() {
 };
 
 exports.default = Home;
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = __webpack_require__(2);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _server = __webpack_require__(3);
+
+var _Home = __webpack_require__(4);
+
+var _Home2 = _interopRequireDefault(_Home);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function () {
+    var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
+
+    return '\n        <html>\n            <head></head>\n            <body>\n                <div id="root">' + content + '</div>\n                <script src="bundle.js"></script>\n            </body>\n        </html>\n    ';
+};
 
 /***/ })
 /******/ ]);
--- server2/src/index.js
+++ server2/src/index.js
@@ -1,4 +1,4 @@
-//common JS importation
+// common JS importation
 // const express = require('express');
 // const React = require('react');
 // const renderToString = require('react-dom/server').renderToString;
@@ -7,9 +7,10 @@
 
 //ES6 / ES2015 import statements
 import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server'; //is a named exports
-import Home from './client/components/Home';
+// import React from 'react';
+// import { renderToString } from 'react-dom/server'; //is a named exports
+// import Home from './client/components/Home';
+import renderer from './helpers/renderer';
 
 const app = express();
 
@@ -17,18 +18,7 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('*', (req,res) => {
-    const content = renderToString(<Home/>);
-
-    const html = `
-        <html>
-            <head></head>
-            <body>
-                <div id="root">${content}</div>
-                <script src="bundle.js"></script>
-            </body>
-        </html>
-    `;
-    res.send(html);
+    res.send(renderer());
 });
 
 app.listen(3000, () => {
