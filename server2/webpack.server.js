1a3e7f0cc2320f0518a8aabb12f56f4ef2201723
Arrashi <arrashilibinuko.19@ichat.sp.edu.sg>
2020-09-30 10:52:06 +0800

    webpack base added

--- server2/webpack.server.js
+++ server2/webpack.server.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base');
 
-module.exports = {
+const config = {
 
     mode: 'none',
 
@@ -19,23 +21,8 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build')
-    },
-
-    //Tell webpack to run babel on every file it runs through
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                options: {
-                    presets: [
-                        'react', 
-                        'stage-0',
-                        ['env', { targets: { browsers: ['last 2 versions'] }}]
-                    ]
-                }
-            }
-        ]
     }
-}
+
+};
+
+module.exports = merge(baseConfig, config);
